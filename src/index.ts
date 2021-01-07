4823c14052f6ac44fadbc3dd8472dd0fbf37e6ce
Pedro Padilha <pedropadilha@me.com>
2021-01-07 01:01:17 -0300

    Basic app done

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['keyboard cat'] }));
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
