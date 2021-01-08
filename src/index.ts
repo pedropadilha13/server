afceacf8e773df3f4890e6ec63b7415226432dc9
Pedro Padilha <pedropadilha@me.com>
2021-01-07 21:16:30 -0300

    App done :)

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { router } from './routes/loginRoutes';
+import './controllers/loginController';
+import './controllers/rootController';
+import { AppRouter } from './AppRouter';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['keyboard cat'] }));
 
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
